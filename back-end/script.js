7079ea6c18189d25f9e94c0808513bf6640ecdcb
Abay <abajburalhy@gmail.com>
2024-04-12 21:16:23 +0500

    img

--- back-end/script.js
+++ back-end/script.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname) +'/index.html');
+});
+ 
+app.listen('3000',()=>{
+
+    console.log(`server is listening on port ${3000}`);
+
+}
+)
